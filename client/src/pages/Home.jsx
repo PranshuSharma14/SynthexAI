ec2d2addb490487c59fd105c3d622f027b0d7837
Pranshu Sharma <pranshusharmaindia@gmail.com>
2025-08-02 01:11:19 +0530

    changes done in client

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
 import Testimonial from '../components/Testimonial'
 import Plan from '../components/Plan'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
